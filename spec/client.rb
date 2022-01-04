95aa73941ffebeb43bec08ad9f3ab58858087b3f
sreedev <sreedevpadmakumar@gmail.com>
2022-01-04 16:50:36 +0530

    added unit test files for payload generator + signature generator

--- spec/client.rb
+++ spec/client.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
-require_relative "../../lib/bitbns.rb"
+require_relative "../lib/bitbns.rb"
 
 describe BitBns::Client do
   before do
@@ -12,7 +12,7 @@ describe BitBns::Client do
   end
 
   describe "#send_request" do
-    it "fetches bitbns ticker when called with `fetch_tickers` option" do
+    it "fetches bitbns ticker when called with `fetch_markets` option" do
       response = @client.send_request("fetch_markets")
 
       assert_equal response.raw.status, 200
@@ -21,6 +21,16 @@ describe BitBns::Client do
       assert_equal response.body.key?("timestamp"), true
       assert_equal response.body.key?("datetime"), true
       assert_equal response.body.key?("nonce"), true
+    end
+
+    it "fetches bitbns markets when called with `fetch_tickers` option" do
+      response = @client.send_request("fetch_tickers")
+      assert_equal response.raw.status, 200
+    end
+
+    it "fetches bitbns orderbook when called with `orderbook` option" do
+      response = @client.send_request("fetch_orderbook", { symbol: "BTCUSDT" })
+      assert_equal response.raw.status, 200
     end
   end
 end
---
+++ spec/utils/payload.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require_relative "../../lib/bitbns/utils/payload.rb"
+
+describe BitBns::Utils::Payload do
+  before do
+  end
+
+  describe "#generate!" do
+  end
+end
